f234b64eabf9b0865fc9be379dd72f7ed6b13588
Innovation4x <innovation4x@gmail.com>
2022-03-23 20:04:52 +0530

    Updated to recent version

--- SelfieSegDLV3.py
+++ SelfieSegDLV3.py
@@ -6,24 +6,50 @@ import numpy as np
 from torchvision import models
 import torch
 from torchvision import transforms
-import portrait_segment_v3 as ps
 
 class SelfieSegDLV3:
     def __init__(self, width=320, height=240):
         self.width = width
         self.height = height
-        self.imgsize = width
-        if width > height:
-            self.imgsize = height
-        self.dev = "cpu"
-        self.model = models.segmentation.deeplabv3_resnet101(pretrained=1).eval()
+        self.dev = "cpu"    #"cuda"
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
+        self.model.eval()
 
     def seg(self, frame):
-        mask = ps.portait_segment(self.model, frame, self.imgsize, dev=self.dev)
+        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        # Comment the Resize and CenterCrop for better inference results
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        input_tensor = preprocess(img)
+        input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
+
+        with torch.no_grad():
+            output = self.model(input_batch)['out'][0]
+        output_predictions = output.argmax(0)
+
+        colors = np.array([(0, 0, 0),  # 0=background
+             # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+             (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0),
+             # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+             (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0),
+             # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+             (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (255, 255, 255),
+             # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+             (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)])
+
+        # plot the semantic segmentation predictions of 21 classes in each color
+        out = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(img.size)
+        out.putpalette(colors)
+        out = np.array(out)
+
+        mask = (255 * (out / 15)).astype("uint8")  # 15=person
         _, mask = cv2.threshold(mask, 128, 255, 0)
 
         return mask
-
 
 if __name__ == "__main__":
     #"""
